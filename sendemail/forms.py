890d46363d2426e7809fbc954e52957baeff6f5d
Skihlz <bakeran2@gmail.com>
2020-03-09 01:38:50 +0200

    modified form font to black

--- sendemail/forms.py
+++ sendemail/forms.py
@@ -2,6 +2,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	from_email = forms.EmailField(widget=forms.EmailInput(attrs={'style': 'width:100%', 'placeholder': 'E-mail'}), required=True)
-	name = forms.CharField(widget=forms.TextInput(attrs={'style': 'width:100%', 'placeholder': 'Name'}), required=True)
-	message = forms.CharField(widget=forms.Textarea(attrs={'style': 'width:100%', 'placeholder': 'Let us know how we can help'}), required=True)
+	from_email = forms.EmailField(widget=forms.EmailInput(attrs={'style': 'width: 100%; color: #000000', 'placeholder': 'E-mail'}), required=True)
+	name = forms.CharField(widget=forms.TextInput(attrs={'style': 'width: 100%; color: #000000', 'placeholder': 'Name'}), required=True)
+	message = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 100%; color: #000000', 'placeholder': 'Let us know how we can help'}), required=True)
