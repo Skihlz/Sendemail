284651e8049147c0d9726c7220f76e94e0b703cc
Skihlz <bakeran2@gmail.com>
2020-03-08 23:13:40 +0200

    Sendemail, first commit

---
+++ sendemail/forms.py
@@ -0,0 +1,7 @@
+# sendemail/forms.py
+from django import forms
+
+class ContactForm(forms.Form):
+	from_email = forms.EmailField(widget=forms.EmailInput(attrs={'style': 'width:100%', 'placeholder': 'E-mail'}), required=True)
+	name = forms.CharField(widget=forms.TextInput(attrs={'style': 'width:100%', 'placeholder': 'Name'}), required=True)
+	message = forms.CharField(widget=forms.Textarea(attrs={'style': 'width:100%', 'placeholder': 'Let us know how we can help'}), required=True)
---
+++ sendemail/urls.py
@@ -0,0 +1,11 @@
+# sendemail/urls.py
+from django.contrib import admin
+from django.urls import path
+
+from .views import emailView, successView
+
+urlpatterns = [
+	path('', emailView, name='email'),
+	path('success/', successView, name='success'),
+]
+
